c64e0be4312a501a5c13b5e1df4a0d1f61510702
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2022-08-18 17:54:52 +0100

    add migration

--- WiredBrainCoffee.EmployeeManager/Data/EmplyeeManagerDbContext.cs
+++ WiredBrainCoffee.EmployeeManager/Data/EmplyeeManagerDbContext.cs
@@ -5,6 +5,7 @@ namespace WiredBrainCoffee.EmployeeManager.Data
 {
     public class EmplyeeManagerDbContext : DbContext
     {
+        public EmplyeeManagerDbContext(DbContextOptions<EmplyeeManagerDbContext> options) : base(options) {}
         public DbSet<Employee> Employees => Set<Employee>();
         public DbSet<Departement> Departements => Set<Departement>();
     }
