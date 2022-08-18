05265017583a2cd108ee0782a95a7963349e5748
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2022-08-18 17:18:35 +0100

    Created Models

---
+++ WiredBrainCoffee.EmployeeManager/Data/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffee.EmployeeManager.Data.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)] 
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(50)] 
+        public string? LastName { get; set; }
+        public bool IsDeveloper { get; set; }
+        [Required] 
+        public int? DepartementId { get; set; }
+        public Departement? Departement { get; set; }
+    }
+}
